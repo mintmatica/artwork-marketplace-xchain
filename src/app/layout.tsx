d29827af2832b4e636342adf49e0a58c9c791a61
mintmatica <mdptrading@gmail.com>
2024-08-11 15:37:22 -0600

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
-import type { Metadata } from "next";
 import { Providers } from "@/components/shared/Providers";
 import { Navbar } from "@/components/shared/Navbar";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Mintmatica.com - abstract artwork, NFT's and random digital artifacts.",
   description: "Marketplace for digital artwork, NFT's, photography, and other interesting assets.",
+  openGraph: {
+    title: "Mintmatica.com - abstract artwork, NFT's and random digital artifacts.",
+    description: "Marketplace for digital artwork, NFT's, photography, and other interesting assets.",
+    images: ["./android-chrome-512x512.png"],
+  },
 };
 
 export default function RootLayout({
